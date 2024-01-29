c8dc8cea30749aaa5f0311cd072212578b2ef1bb
LazaroQueiroz <lazaro.queiroz.nascimento@ccc.ufcg.edu.br>
2024-01-29 16:58:59 -0300

    First commit. this commit contains the first part of the project: the Go API.

---
+++ goapi/go.mod
@@ -0,0 +1,5 @@
+module gihub.com/devfullcycle/imersao17/goapi
+
+go 1.18
+
+require github.com/google/uuid v1.6.0
---
+++ goapi/internal/service/category_service.go
@@ -0,0 +1,5 @@
+package service
+
+import "github.com/devfullcycle/imersao17/goapi/internal/database"
+
+type Cate
